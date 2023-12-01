287dd1a8250fe52549c2ce718fe954cfa5305ea3
David Stark <dave@davidstark.name>
2023-12-01 16:26:26 +0000

    Release v1.2, update deps
    Python 3.11 -> 3.12
    protobuff 4.22.0 -> 4.25.1
    pyobjc* 9.0.1 -> 9.2

--- csshi.rb
+++ csshi.rb
@@ -4,19 +4,19 @@ class Csshi < Formula
 
   desc "csshi - ClusterSSH for iTerm2"
   homepage "https://github.com/ilikejam/csshi"
-  url "https://github.com/ilikejam/csshi/archive/refs/tags/v1.1.tar.gz"
-  sha256 "72818b531e54b26d4ae3f4ff1a21b9bab8b65161a5565143edf088373f5428f8"
+  url "https://github.com/ilikejam/csshi/archive/refs/tags/v1.2.tar.gz"
+  sha256 "ddc3054a1fbbbc58a78acd1ba49b41932b6207755f88d3b820cb77a722cb45a8"
   license "GPL-2.0-only"
 
-  depends_on "python@3.11"
+  depends_on "python@3.12"
 
   @@pip_pkgs = [
     "iterm2==2.7",
-    "protobuf==4.22.0",
+    "protobuf==4.25.1",
     "websockets==10.4",
-    "pyobjc==9.0.1",
-    "pyobjc-core==9.0.1",
-    "pyobjc-framework-Cocoa==9.0.1",
+    "pyobjc==9.2",
+    "pyobjc-core==9.2",
+    "pyobjc-framework-Cocoa==9.2",
   ]
 
   def install
