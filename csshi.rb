13c59e449dc45b8b15ce0dfe490e1a6a33b7133a
David Stark <dave@davidstark.name>
2024-06-03 03:30:48 +0100

    Lint csshi.rb

--- csshi.rb
+++ csshi.rb
@@ -1,35 +1,42 @@
+# frozen_string_literal: true
+
+# Homebrew formula for csshi
 class Csshi < Formula
   include Language::Python::Virtualenv
   include Language::Python::Shebang
 
-  desc "csshi - ClusterSSH for iTerm2"
-  homepage "https://github.com/ilikejam/csshi"
-  url "https://github.com/ilikejam/csshi/archive/refs/tags/v1.4.tar.gz"
-  sha256 "b8d2c4f9e3a6770e694ae59ebdd8d0d0f83a7ce69da12dd300e6150402608656"
-  license "GPL-2.0-only"
+  desc 'csshi - ClusterSSH for iTerm2'
+  homepage 'https://github.com/ilikejam/csshi'
+  url 'https://github.com/ilikejam/csshi/archive/refs/tags/v1.4.tar.gz'
+  sha256 'b8d2c4f9e3a6770e694ae59ebdd8d0d0f83a7ce69da12dd300e6150402608656'
+  license 'GPL-2.0-only'
 
-  depends_on "python@3.12"
+  depends_on 'python@3.12'
 
-  @@pip_pkgs = [
-    "iterm2==2.7",
-    "protobuf==4.25.1",
-    "websockets==10.4",
-    "pyobjc==9.2",
-    "pyobjc-core==9.2",
-    "pyobjc-framework-Cocoa==9.2",
+  @pip_pkgs = [
+    'iterm2==2.7',
+    'protobuf==4.25.1',
+    'websockets==10.4',
+    'pyobjc==9.2',
+    'pyobjc-core==9.2',
+    'pyobjc-framework-Cocoa==9.2'
   ]
 
+  class << self
+    attr_reader :pip_pkgs
+  end
+
   def install
-    venv = virtualenv_create(libexec, "python3")
-    @@pip_pkgs.each do |pkg|
+    venv = virtualenv_create(libexec, 'python3')
+    Csshi.pip_pkgs.each do |pkg|
       venv.pip_install pkg
     end
-    bin.install "csshi"
+    bin.install 'csshi'
     rw_info = python_shebang_rewrite_info("#{libexec}/bin/python3")
-    rewrite_shebang rw_info, bin/"csshi"
+    rewrite_shebang rw_info, 'bin/csshi'
   end
 
   test do
-    system bin/"csshi", "--help"
+    system 'bin/csshi', '--help'
   end
 end
